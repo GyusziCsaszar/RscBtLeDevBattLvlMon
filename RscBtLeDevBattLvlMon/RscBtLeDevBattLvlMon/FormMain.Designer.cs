17d179924e047552ca6e93088ef2982385e5725b
GyusziCsaszar <gyuszi.csaszar@gmail.com>
2020-09-03 15:27:06 +0200

    Initial commit.

---
+++ RscBtLeDevBattLvlMon/RscBtLeDevBattLvlMon/FormMain.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace RscBtLeDevBattLvlMon
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(12, 12);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 0;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // lbLog
+            // 
+            this.lbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.ItemHeight = 15;
+            this.lbLog.Location = new System.Drawing.Point(12, 41);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(515, 394);
+            this.lbLog.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 450);
+            this.Controls.Add(this.lbLog);
+            this.Controls.Add(this.btnQuery);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.Shown += new System.EventHandler(this.FormMain_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.ListBox lbLog;
+    }
+}
+
---
+++ RscBtLeDevBattLvlMon/RscBtLeDevBattLvlMon/FormMain.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// SRC: https://software.intel.com/content/www/us/en/develop/articles/using-winrt-apis-from-desktop-applications.html
+// ATTN: There are differences between WinRT 8.1 and WinRT 10!!!
+
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+
+// SRC: https://www.andreasjakl.com/read-battery-level-bluetooth-le-devices/
+
+namespace RscBtLeDevBattLvlMon
+{
+    public partial class FormMain : Form
+    {
+        BluetoothLEAdvertisementWatcher BleWatcher = null;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+            // TODO...
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            lbLog.Items.Clear();
+
+            //
+            ////
+            //
+
+            //var localAdapter = await BluetoothAdapter.GetDefaultAsync();
+
+            Windows.Foundation.IAsyncOperation<BluetoothAdapter> tsk;
+
+            tsk = BluetoothAdapter.GetDefaultAsync();
+
+            while (tsk.Status == Windows.Foundation.AsyncStatus.Started)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+
+            var localAdapter = tsk.GetResults();
+
+            lbLog.Items.Add("Low energy supported? -> " + localAdapter.IsLowEnergySupported);
+            lbLog.Items.Add("Central role supported? -> " + localAdapter.IsCentralRoleSupported);
+            lbLog.Items.Add("Pheripherial role supported? -> " + localAdapter.IsPeripheralRoleSupported);
+
+            //
+            ////
+            //
+
+            // SRC: https://stackoverflow.com/questions/43568096/frombluetoothaddressasync-never-returns-on-windows-10-creators-update-in-wpf-app
+
+            BleWatcher = new BluetoothLEAdvertisementWatcher
+            {
+                ScanningMode = BluetoothLEScanningMode.Active
+            };
+            BleWatcher.Received += Watcher_Received;
+            BleWatcher.Start();
+        }
+
+        private /*async*/ void Watcher_Received(BluetoothLEAdvertisementWatcher sender,
+                                           BluetoothLEAdvertisementReceivedEventArgs args)
+        {
+            lbLog.Items.Add("Bluetooth LE Device Advertisement Local Name -> " + args.Advertisement.LocalName);
+            Windows.Foundation.IAsyncOperation<BluetoothLEDevice> tsk1;
+
+            tsk1 = BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
+
+            while (tsk1.Status == Windows.Foundation.AsyncStatus.Started)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+
+            var bluetoothLeDevice = tsk1.GetResults();
+
+            lbLog.Items.Add("                    Address -> " + args.BluetoothAddress);
+            lbLog.Items.Add("          Connection Status -> " + bluetoothLeDevice.ConnectionStatus);
+            lbLog.Items.Add("                       Name -> " + bluetoothLeDevice.Name);
+            lbLog.Items.Add("                  Device ID -> " + bluetoothLeDevice.DeviceId);
+
+            //
+            ////
+            //
+
+            Windows.Foundation.IAsyncOperation<Windows.Devices.Bluetooth.GenericAttributeProfile.GattDeviceServicesResult> tsk2;
+
+            tsk2 = bluetoothLeDevice.GetGattServicesAsync();
+
+            while (tsk2.Status == Windows.Foundation.AsyncStatus.Started)
+            {
+                System.Threading.Thread.Sleep(100);
+            }
+
+            var gattServices = tsk2.GetResults();
+
+            foreach (var curService in gattServices.Services)
+            {
+                lbLog.Items.Add("  Service: " + curService.Uuid);
+            }
+
+            // TODO...
+
+            bluetoothLeDevice.Dispose();
+        }
+    }
+}
